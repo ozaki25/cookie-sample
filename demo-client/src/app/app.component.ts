0d6c11822e764148584547e2d7325d4836065831
ozaki <fs65ugumori@yahoo.co.jp>
2020-02-17 22:48:58 +0900

    udpate demo client

--- demo-client/src/app/app.component.ts
+++ demo-client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
@@ -7,10 +7,12 @@ import { catchError, map } from 'rxjs/operators';
   selector: 'app-root',
   template: `
     <h1>Welcome to {{ title }}!</h1>
+    <button (click)="onClickPost()">1.POST</button>
+    <button (click)="onClickGet()">2.GET</button>
   `,
   styles: [],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'demo-client';
 
   private httpOptions = {
@@ -21,13 +23,36 @@ export class AppComponent implements OnInit {
   };
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
+  onClickPost() {
     this.post().subscribe(res => console.log(res));
+  }
+
+  onClickGet() {
+    this.get().subscribe(res => console.log(res));
   }
 
   post() {
     return this.http
       .post('http://localhost:8080/', null, {
+        ...this.httpOptions,
+        observe: 'response',
+        withCredentials: true,
+      })
+      .pipe(
+        map(response => {
+          console.log(document.cookie);
+          console.log(response);
+          console.log(response && response.headers);
+          console.log(response && response.headers && response.headers.keys());
+          return response.body;
+        }),
+        catchError(this.handleError),
+      );
+  }
+
+  get() {
+    return this.http
+      .get('http://localhost:8080/', {
         ...this.httpOptions,
         observe: 'response',
         withCredentials: true,
