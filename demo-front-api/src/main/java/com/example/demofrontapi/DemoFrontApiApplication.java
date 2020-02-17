4ab9016319f0fa76fe3ef1b2aefe0bef62d60a36
ozaki <fs65ugumori@yahoo.co.jp>
2020-02-17 22:25:51 +0900

    update front api

--- demo-front-api/src/main/java/com/example/demofrontapi/DemoFrontApiApplication.java
+++ demo-front-api/src/main/java/com/example/demofrontapi/DemoFrontApiApplication.java
@@ -1,12 +1,24 @@
 package com.example.demofrontapi;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 
 @SpringBootApplication
@@ -17,11 +29,24 @@ public class DemoFrontApiApplication {
     SpringApplication.run(DemoFrontApiApplication.class, args);
   }
 
+  @Autowired
+  private RestTemplateBuilder restTemplateBuilder;
+
   @PostMapping
-  public ResponseEntity<String> login() {
-    RestTemplate restTemplate = new RestTemplate();
-    String result = restTemplate.postForObject("http://localhost:8082", null, String.class);
-    return ResponseEntity.ok().body(result);
+  public ResponseEntity<?> login(@RequestHeader MultiValueMap<String, String> headers, @CookieValue(name = "sessionId", required = false) String sessionId) {
+    System.out.println("###################");
+    System.out.println(sessionId);
+    System.out.println("###################");
+
+    RestTemplate restTemplate = restTemplateBuilder.build();
+    RequestEntity<String> requestEntity = null;
+    URI uri = UriComponentsBuilder
+      .fromUriString("http://localhost:8082")
+      .build()
+      .toUri();
+    requestEntity = new RequestEntity<String>(null, headers, HttpMethod.POST, uri);
+
+    return restTemplate.exchange(requestEntity, Object.class);
   }
 
 }
