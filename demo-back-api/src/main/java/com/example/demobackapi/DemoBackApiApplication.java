5d7594901cabe75c7364955542d2e05193025fe9
ozaki <fs65ugumori@yahoo.co.jp>
2020-02-17 20:05:47 +0900

    init

---
+++ demo-back-api/src/main/java/com/example/demobackapi/DemoBackApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBackApiApplication.class, args);
+	}
+
+}
---
+++ demo-front-api/src/main/java/com/example/demofrontapi/DemoFrontApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demofrontapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFrontApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFrontApiApplication.class, args);
+	}
+
+}
