c30c6269ac7745b9dc1d38b7539b4f61eaa0c44d
ozaki <fs65ugumori@yahoo.co.jp>
2020-02-17 22:05:32 +0900

    update front api

--- demo-front-api/src/main/java/com/example/demofrontapi/DemoFrontApiApplication.java
+++ demo-front-api/src/main/java/com/example/demofrontapi/DemoFrontApiApplication.java
@@ -3,47 +3,26 @@ package com.example.demofrontapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpCookie;
-import org.springframework.http.ResponseCookie;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 @RestController
 @CrossOrigin(allowCredentials = "true")
 public class DemoFrontApiApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(DemoFrontApiApplication.class, args);
   }
 
   @PostMapping
-  public ResponseEntity<Hello> login(@RequestParam MultiValueMap<String, String> params) {
-        Hello hello = new Hello();
-        hello.setHello("Hello!!!!!");
-        HttpCookie cookie = ResponseCookie.from("IwSessionId", "xxxxxxxxxxxxxxxxxxx").path("/").build();
-        return ResponseEntity
-          .ok()
-          .header(HttpHeaders.SET_COOKIE, cookie.toString())
-          .body(hello);
-    }
+  public ResponseEntity<String> login() {
+    RestTemplate restTemplate = new RestTemplate();
+    String result = restTemplate.postForObject("http://localhost:8082", null, String.class);
+    return ResponseEntity.ok().body(result);
   }
+
 }
 
-class Hello {
-  private String hello;
-
-  public void setHello(String hello) {
-    this.hello = hello;
-  }
-
-  public String getHello() {
-    return this.hello;
-  }
-}
